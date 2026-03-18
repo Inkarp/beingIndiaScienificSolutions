e16800214f449e09d4bb4eabef5d7b96e56795a0
Sharath Gounikadi <sharath@inkarp.co.in>
2026-03-18 12:26:37 +0530

    Fixed the layout according to the new UI developement through the vercel fo rthe deployment

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,10 @@
-import Hero from './home/Hero';
-import About from './home/About';
-import Blogs from './home/Blogs';
-import Customers from './home/Customers';
-import Products from './home/Products';
-import Testimonials from './home/Testimonials';
-import HeroNew from './home/HeroNew';
+
 import HeroOne from './home/HeroOne';
 
 
 export default function Home() {
   return (
-    <div className="space-y-2">
-      {/* <HeroNew />
-      <HeroOne />
-      <Hero />    
-      <Products />
-      <About />
-      <Customers />
-      <Blogs />
-      <Testimonials />   */}
+    <div className="">
       <HeroOne />
     </div>
   );
